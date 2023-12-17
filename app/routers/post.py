70cef51dc94cee3b5a8422d0acc23997fdd40acf
Admin <iykmorris@gmail.com>
2023-12-17 22:11:14 +0300

    API Validation

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from typing import List
-from .. import models, schemas
+from .. import models, schemas, oauth2
 from .. database import get_db
 
 router = APIRouter(
@@ -17,8 +17,10 @@ def get_posts(db: Session = Depends(get_db)):
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
+def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), user_id: int = 
+                Depends(oauth2.get_current_user)):
 
+    print (user_id)
     new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
