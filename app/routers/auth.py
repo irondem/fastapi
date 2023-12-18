dcb9287958a0c305ffe7c18256323b2375f04bf4
irondem <iykmorris@gmail.com>
2023-12-18 16:58:24 +0300

    return logged in user

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -6,7 +6,7 @@ from .. import database, schemas, models, utils, oauth2
 
 router  = APIRouter(tags=['Authentication'])
 
-@router.post('/login')
+@router.post('/login', response_model= schemas.Token)
 def login( user_credentials:OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)): 
 
     #For OAuth2PasswordRequestForm  username = email
@@ -25,3 +25,5 @@ def login( user_credentials:OAuth2PasswordRequestForm = Depends(), db: Session =
     access_token = oauth2.create_access_token(data = {"user_id": user.id})
 
     return {"access_token": access_token, "token_type":"bearer"}
+
+#Test
