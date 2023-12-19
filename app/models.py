46c7dfbbff6ce193bdd4bfde1c6998a99533e442
irondem <iykmorris@gmail.com>
2023-12-19 11:46:49 +0300

    Foreign Key relationship

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.sql.sqltypes import TIMESTAMP 
-from sqlalchemy.sql.expression  import text
+from sqlalchemy.sql.expression  import text 
 from .database import Base
 
 class Post(Base):
@@ -12,8 +12,11 @@ class Post(Base):
     published = Column(Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP( timezone=True), nullable=False, server_default= text('now()'))
 
+    owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+
+
 class User(Base):
-    __tablename__ = "Users"
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
--- app/routers/post.py
+++ app/routers/post.py
@@ -22,7 +22,7 @@ def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), current
                 Depends(oauth2.get_current_user)):
 
     print (current_user.email)
-    new_post = models.Post(**post.dict())
+    new_post = models.Post(owner_id = current_user.id, **post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
