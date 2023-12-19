ad7a03616135a0d0daf99e5f0c9181d3cca4011d
irondem <iykmorris@gmail.com>
2023-12-19 14:46:58 +0300

    Update/Delete Authorization

--- app/routers/post.py
+++ app/routers/post.py
@@ -45,13 +45,18 @@ def get_post(id: int, db: Session = Depends(get_db), current_user: int =
 def delete_post(id: int, db: Session = Depends(get_db), current_user: int = 
                 Depends(oauth2.get_current_user)):
 
-    post = db.query(models.Post).filter(models.Post.id == id)
+    post_query = db.query(models.Post).filter(models.Post.id == id)
 
-    if post.first() == None:
+    post = post_query.first()
+
+    if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail= f"Post with id: {id} does not exist")
     
-    post.delete(synchronize_session=False)
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
+
+    post_query.delete(synchronize_session=False)
     db.commit()
     
     return Response(status_code= status.HTTP_204_NO_CONTENT)
@@ -63,11 +68,16 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
     
     post_query = db.query(models.Post).filter(models.Post.id == id)
 
+
+
     post = post_query.first()
 
     if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail= f"post with id: {id} does not exist")
+    
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
     
 
     post_query.update(updated_post.dict(), synchronize_session=False)
